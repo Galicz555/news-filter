6b7d06263cc2142db8b643dd356fb291463e2d10
Galicz Mihály <mihaly.galicz@aliasinnov.com>
2024-10-12 09:30:22 +0200

    fix: removed js files

--- utils/szövegek/fertőtlenít.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fertőtlenítsd_őket = exports.fertőtlenítsd = void 0;
-var Array_1 = require("fp-ts/Array");
-var fertőtlenítsd = function (szöveg) { return szöveg.trim(); };
-exports.fertőtlenítsd = fertőtlenítsd;
-exports.fertőtlenítsd_őket = (0, Array_1.map)(exports.fertőtlenítsd);
--- utils/áramlatok/csiribu.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.gyűjtsd_ki_célzottan_és_fertőtlenítsd_őket = exports.gyűjtsd_ki_és_fertőtlenítsd_őket = void 0;
-var function_1 = require("fp-ts/lib/function");
-var m_veletek_1 = require("../csiribu/m\u0171veletek");
-var fert_tlen_t_1 = require("../sz\u00F6vegek/fert\u0151tlen\u00EDt");
-exports.gyűjtsd_ki_és_fertőtlenítsd_őket = (0, function_1.flow)(m_veletek_1.gyűjtsd_ki, fert_tlen_t_1.fertőtlenítsd_őket);
-exports.gyűjtsd_ki_célzottan_és_fertőtlenítsd_őket = (0, function_1.flow)(m_veletek_1.gyűjtsd_ki_célzottan, fert_tlen_t_1.fertőtlenítsd_őket);
