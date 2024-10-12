2e88accbd3d0200e8bf509d5624b0a7a56f76033
Galicz Mihály <mihaly.galicz@aliasinnov.com>
2024-10-12 14:54:25 +0200

    feat: added date to portfolio articles

--- web/mágikus_formulák/portfolio.ts
+++ web/mágikus_formulák/portfolio.ts
@@ -6,6 +6,7 @@ import {
 } from '../../utils/áramlatok/csiribu';
 
 export type Portfolio = {
+  időpont: string[];
   főcímek: string[];
   alcímek: string[];
   szöveg: string[];
@@ -13,10 +14,12 @@ export type Portfolio = {
 };
 
 export const portfolio = async ($: cheerio.CheerioAPI) => {
+  const időpont = gyűjtsd_ki_célzottan_és_fertőtlenítsd_őket($, 'time', '.d-block');
   const főcímek = gyűjtsd_ki_és_fertőtlenítsd_őket($, 'h1');
-  const alcímek = gyűjtsd_ki_célzottan_és_fertőtlenítsd_őket($, 'h2', 'pfarticle');
-  const szöveg = gyűjtsd_ki_célzottan_és_fertőtlenítsd_őket($, 'p', 'pfarticle');
+  const alcímek = gyűjtsd_ki_célzottan_és_fertőtlenítsd_őket($, 'h2', '.pfarticle');
+  const szöveg = gyűjtsd_ki_célzottan_és_fertőtlenítsd_őket($, 'p', '.pfarticle');
   return {
+    időpont,
     főcímek,
     alcímek,
     szöveg,
