88e5761734fd19a0c235181fe8e7c2da864b5608
Galicz Mihály <mihaly.galicz@aliasinnov.com>
2024-11-27 06:10:41 +0100

    refact

--- fő_folyamatok/cikkÍró.ts
+++ fő_folyamatok/cikkÍró.ts
@@ -3,44 +3,38 @@ import { alakítsd_JSON_szöveggé } from '@/utils/rendszer/fájl';
 
 export const írj_cikkeket = async (könyvek: Promise<string[][]>) =>
   (await könyvek).forEach((könyv, index) => {
-    könyv.forEach((oldal, i) => {
-      írj_cikket(oldal, index * 10 + i);
-    });
+    könyv.forEach((oldal, i) => írj_cikket(dolgozd_fel_az_oldalt(oldal, '¤'), index * 10 + i));
   });
 
-function írj_cikket(szöveg: string, index: number) {
-  const feldolgozott = dolgozd_fel_az_értelmezett_cikket(szöveg, '¤');
+const írj_cikket = (
+  szöveg: { cikkSzöveg: string; értékelés: Értékelés | undefined },
+  index: number,
+) =>
   hozz_létre_helyi_tárolót(
     'cikkek',
     'enlightment',
     index,
   )(
     alakítsd_JSON_szöveggé({
-      értékelés: feldolgozott?.értékelés,
-      szöveg: feldolgozott?.cikkSzöveg,
+      értékelés: szöveg?.értékelés,
+      szöveg: szöveg?.cikkSzöveg,
     }),
   );
+
+function dolgozd_fel_az_oldalt(szöveg: string, specJel: string) {
+  const match = szöveg.replace(new RegExp(specJel, 'g'), '"').match(/({[^}]*})\s*/);
+
+  return {
+    cikkSzöveg: szöveg.replace(match ? match[0] : '', ''),
+    értékelés: match ? (JSON.parse(match[1].replace(/(\w+):/g, '"$1":')) as Értékelés) : undefined,
+  };
 }
 
-function dolgozd_fel_az_értelmezett_cikket(szöveg: string, specJel: string) {
-  const regex = /({[^}]*})\s*/;
-  const match = szöveg.replace(new RegExp(specJel, 'g'), '"').match(regex);
-
-  let output: {
-    cikkSzöveg?: string;
-    értékelés?: Record<string, number>;
-  } = {};
-
-  output.cikkSzöveg = szöveg.replace(match ? match[0] : '', '');
-
-  if (match) {
-    const jsonString = match[1];
-    try {
-      output.értékelés = JSON.parse(jsonString.replace(/(\w+):/g, '"$1":'));
-    } catch (error) {
-      console.error('Invalid JSON format:', error);
-    }
-  }
-
-  return output;
+interface Értékelés {
+  jóságosság: number;
+  filozófikusság: number;
+  viccesség: number;
+  meglepőség: number;
+  alaposság: number;
+  pénzügyi_haszon: number;
 }
